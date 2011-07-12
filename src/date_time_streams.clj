e732a1b129346e649321bd96d9b55e04252783dc
AlexBaranosky <alexander.baranosky@gmail.com>
2011-07-11 21:02:57 -0400

    removed some code by using x-days stream for x-weeks schedule parsing

--- src/date_time_streams.clj
+++ src/date_time_streams.clj
@@ -1,24 +1,21 @@
 (ns date-time-streams
   (:use [clojure.contrib.seq-utils :only (find-first)])
   (:import [org.joda.time DateMidnight]))
-  
+
 (defn today+all-future-dates []
    (iterate #(.plusDays % 1) (DateMidnight.)))
-    
+
 (defn- next-day-of-week-in-future [day-num]
   (find-first #(= day-num (.. % dayOfWeek get)) (today+all-future-dates)))
 
 (defn day-of-week-stream [day-num]
   (iterate #(.plusDays % 7) (next-day-of-week-in-future day-num)))
- 
+
 (defn- next-day-of-month-in-future [day-of-month]
   (find-first #(= day-of-month (.. % dayOfMonth get)) (today+all-future-dates)))
-  
+
 (defn day-of-month-stream [day-of-month-num]
   (iterate #(.plusMonths % 1) (next-day-of-month-in-future day-of-month-num)))
-  
-(defn every-x-weeks-stream [start-date x-weeks]
-  (iterate #(.plusDays % (* 7 x-weeks)) start-date))
 
 (defn every-x-days-stream [start-date x-days]
   (iterate #(.plusDays % x-days) start-date))
--- src/reminder_parsing.clj
+++ src/reminder_parsing.clj
@@ -9,27 +9,27 @@
   
 (defn kind-of-schedule [s] 
   (cond (re-find day-of-month-identifier-regex s) :day-of-month
-        (re-find every-x-weeks-regex s) :every-x-weeks  
-        (re-find every-x-days-regex s) :every-x-days 
-        (re-find date-regex s)          :date   
-        (re-find day-of-week-regex s)   :day-of-week		
-        (re-find everyday-regex s)      :everyday    
+        (re-find every-x-weeks-regex s) :every-x-weeks
+        (re-find every-x-days-regex s) :every-x-days
+        (re-find date-regex s)          :date
+        (re-find day-of-week-regex s)   :day-of-week
+        (re-find everyday-regex s)      :everyday
 	:else                           :unrecognized-format))
-		
+
 (defmulti parse-schedule kind-of-schedule)
 
-(defmethod parse-schedule :day-of-month [s] 
+(defmethod parse-schedule :day-of-month [s]
    (let [[ordinals-part] (re-captures day-of-month-identifier-regex s)
          ordinals (map ordinal-to-int (re-match-seq ordinal-regex ordinals-part))]
-     (map day-of-month-stream ordinals)))   
-	
-;; TODO: how shall we handle the fact that technically the day-of-week is unneeded, yet the reminder line phrasing is funny without it!?!?...	
+     (map day-of-month-stream ordinals)))
+
+;; TODO: how shall we handle the fact that technically the day-of-week is unneeded, yet the reminder line phrasing is funny without it!?!?...
 (defmethod parse-schedule :every-x-weeks [s]
   (let [[ordinal day-of-week month day year] (re-captures every-x-weeks-regex s)
         start-date (DateMidnight. (Integer/parseInt year) (Integer/parseInt month) (Integer/parseInt day))
 	x-weeks (ordinal-to-int ordinal)]
-    (every-x-weeks-stream start-date x-weeks)))
-		
+    (every-x-days-stream start-date (* 7 x-weeks))))
+
 (defmethod parse-schedule :every-x-days [s]
   (let [[ordinal month day year] (re-captures every-x-days-regex s)
         start-date (DateMidnight. (Integer/parseInt year) (Integer/parseInt month) (Integer/parseInt day))
