32618fb9f0fded6823ed2dd7b514dde9783bde0f
Alex Baranosky <alexander.baranosky@gmail.com>
2011-09-25 17:31:19 -0400

    fixed indentation

--- src/fact/reminder_email_history_facts.clj
+++ src/fact/reminder_email_history_facts.clj
@@ -18,20 +18,20 @@
   (do-at today
     (given-three-reminders-sent-on today)
     (history/num-reminders-sent-today)) => 3
-    (provided
-      (history/history-file) => fake-history-file))
+  (provided
+    (history/history-file) => fake-history-file))
 
 (fact "resets the number of reminders already sent to 0 on each new day"
   (do-at today
     (given-three-reminders-sent-on yesterday)
     (history/num-reminders-sent-today)) => 0
-    (provided
-      (history/history-file) => fake-history-file))
+  (provided
+    (history/history-file) => fake-history-file))
 
 (fact "stores and retrieves a reminder's count and day of persisting"
   (do-at today
     (given-three-reminders-sent-on yesterday)
     (history/record-num-reminders-sent-today 5)
     (history/num-reminders-sent-today)) => 5
-    (provided
-      (history/history-file) => fake-history-file))
+  (provided
+    (history/history-file) => fake-history-file))
