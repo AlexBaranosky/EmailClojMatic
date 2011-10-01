3b71cb43e4a4e130239a9114a8398ff39a44cc41
Alex Baranosky <alexander.baranosky@gmail.com>
2011-10-01 15:45:22 -0400

    more test refactoring

--- src/fact/date_time_streams_facts.clj
+++ src/fact/date_time_streams_facts.clj
@@ -5,40 +5,24 @@
   (:use [utility :only (third fourth)])
   (:use midje.sweet))
 
-
 (fact "makes a stream of xth day of the month"
-  (do-at (DateMidnight. 2011 6 15)
-    (take 2 (day-of-month-stream 1))) => [(DateMidnight. 2011 7 1) (DateMidnight. 2011 8 1)])
+  (do-at (DateMidnight. 2011 10 15)
+    (take 4 (day-of-month-stream 1))) => [(DateMidnight. 2011 11 1)
+                                          (DateMidnight. 2011 12 1)
+                                          (DateMidnight. 2012 1 1)
+                                          (DateMidnight. 2012 2 1)])
 
 (fact "makes stream of month+day, for every year into infinite - using every 4th year as leap year"
   (do-at (DateMidnight. 2011 6 15)
-      (take 5 (month+day-stream 9 1))) => [(DateMidnight. 2011 9 1)
-                                           (DateMidnight. 2012 9 1)
-                                           (DateMidnight. 2013 9 1)
-                                           (DateMidnight. 2014 9 1)
-                                           (DateMidnight. 2015 9 1)])
-
-;(tabular
-;  (fact "makes a stream of given day of the month"
-;   (?nth (day-of-month-stream 1)) => ?date-time)
-
-;	?nth   ?date-time
-;	first  (DateMidnight. 2011 11 1)
-;	second (DateMidnight. 2011 12 1)
-;	third  (DateMidnight. 2012 1 1)
-;	fourth (DateMidnight. 2012 2 1))
-
-;(tabular
-;  (fact "day-stream gives lazy infinite seqs of datetimes on the given day of the week"
-;    (do-at (DateMidnight. 2011 6 1)
-;     (?nth (day-of-week-stream (:tuesdays day-nums)))) => ?date-time)
-;
-;	?nth   ?date-time
-;	first  (DateMidnight. 2011 12 27)
-;	second (DateMidnight. 2012 1 3)
-;	third  (DateMidnight. 2012 1 10)
-;	fourth (DateMidnight. 2012 1 17))
+    (take 5 (month+day-stream 9 1))) => [(DateMidnight. 2011 9 1)
+                                         (DateMidnight. 2012 9 1)
+                                         (DateMidnight. 2013 9 1)
+                                         (DateMidnight. 2014 9 1)
+                                         (DateMidnight. 2015 9 1)])
 
 (facts "day-stream gives lazy infinite seqs of datetimes on the given day of the week"
-  (do-at (DateMidnight. 2011 6 1)
-    (take 2 (day-of-week-stream (:fridays day-nums)))) => [(DateMidnight. 2011 6 3) (DateMidnight. 2011 6 10)])
+  (do-at (DateMidnight. 2011 12 25)
+    (take 4 (day-of-week-stream (:tuesdays day-nums)))) => [(DateMidnight. 2011 12 27)
+                                                            (DateMidnight. 2012 1 3)
+                                                            (DateMidnight. 2012 1 10)
+                                                            (DateMidnight. 2012 1 17)])
--- src/fact/load_reminders_integration.clj
+++ src/fact/load_reminders_integration.clj
@@ -1,16 +1,12 @@
 (ns fact.load-reminders-integration
   (:require core)
   (:import [org.joda.time DateMidnight])
-  (:use [utility :only (resource)])
+  (:use [utility :only (resource only)])
   (:use [joda-time-cop :only [do-at]])
   (:use clojure.test)
   (:use midje.sweet))
 
 (fact "can load due reminders from a given file"
   (do-at (DateMidnight. 2011 6 1)
-    (= (core/load-due-reminders (resource "test_reminder_file.txt"))
-	    [{
-               :message "message 1"
-               :schedule [(DateMidnight. 2011 6 4)]
-               :days-in-advance 8
-             }])) => true)
+    (only (core/load-due-reminders (resource "test_reminder_file.txt")))) =>
+      { :message "message 1" :schedule [(DateMidnight. 2011 6 4)] :days-in-advance 8 })
