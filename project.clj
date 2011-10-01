254b49f8cfd90541943357bc5847020b6f5f3661
Alex Baranosky <alexander.baranosky@gmail.com>
2011-10-01 01:16:17 -0400

    upgraded to jodatime 2.0

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :dev-dependencies [[midje "1.2-alpha4"]]
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [joda-time "1.6"]
+                 [joda-time "2.0"]
 	 	 [org.apache.commons/commons-email "1.2"]]
   :main email-cloj-matic)
