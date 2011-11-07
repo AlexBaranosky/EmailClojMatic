e3b17e1221b6cca551291eb41a4513c3c574dd46
Alex Baranosky <alexander.baranosky@gmail.com>
2011-11-06 23:44:27 -0500

    cleaned it up

--- src/email_cloj_matic.clj
+++ src/email_cloj_matic.clj
@@ -5,7 +5,8 @@
   
 (defn -main [& names+email-addresses]
   {:pre [(even? (count names+email-addresses))]}
-  
-  (let [to-recipient (fn [[name email-address]] (->EmailRecipient name email-address))
-        recipients (map to-recipient (partition 2 names+email-addresses))] 
-    (run-reminders recipients)))
+
+  (->> names+email-addresses
+       (partition 2)
+       (map (partial apply ->EmailRecipient))
+       run-reminders))
