7d4d08a74bf29dda78d8f297be6e2838eab88f98
Alex Baranosky <alexander.baranosky@gmail.com>
2011-11-14 23:09:11 -0500

    trying out a let(fn) approach

--- src/date_time_streams.clj
+++ src/date_time_streams.clj
@@ -5,25 +5,30 @@
 (defn today+all-future-dates []
    (iterate #(.plusDays % 1) (DateMidnight.)))
 
-(defn- next-day-of-week-in-future [day-num]
-  (find-first #(= day-num (.. % dayOfWeek get)) (today+all-future-dates)))
-
 (defn day-of-week-stream [day-num]
-  (iterate #(.plusDays % 7) (next-day-of-week-in-future day-num)))
-
-(defn- next-day-of-month-in-future [day-of-month]
-  (find-first #(= day-of-month (.. % dayOfMonth get)) (today+all-future-dates)))
+  (letfn [(next-day-of-week-in-future [day-num]
+           (find-first #(= day-num (.. % dayOfWeek get)) (today+all-future-dates)))]
+    (iterate #(.plusDays % 7) (next-day-of-week-in-future day-num))))
 
 (defn day-of-month-stream [day-of-month-num]
-  (iterate #(.plusMonths % 1) (next-day-of-month-in-future day-of-month-num)))
+  (letfn [(next-day-of-month-in-future [day-of-month]
+           (find-first #(= day-of-month (.. % dayOfMonth get)) (today+all-future-dates)))]
+    (iterate #(.plusMonths % 1) (next-day-of-month-in-future day-of-month-num))))
 
 (defn every-x-days-stream [start-date x-days]
   (iterate #(.plusDays % x-days) start-date))
 
-(defn- next-date-in-future [month day-of-month]
-  (find-first #(and (= month (.. % monthOfYear get)) (= day-of-month (.. % dayOfMonth get))) (today+all-future-dates)))
-
 (defn month+day-stream [month day-of-month]
-  (iterate #(.plusYears % 1) (next-date-in-future month day-of-month)))
+  (letfn [(next-date-in-future [month day-of-month]
+            (find-first #(and (= month (.. % monthOfYear get))
+                              (= day-of-month (.. % dayOfMonth get)))
+                        (today+all-future-dates)))]
+    (iterate #(.plusYears % 1) (next-date-in-future month day-of-month))))
 
-(defonce day-nums { :mondays 1 :tuesdays 2 :wednesdays 3 :thursdays 4 :fridays 5 :saturdays 6 :sundays 7 })
+(defonce day-nums { :mondays 1
+                    :tuesdays 2
+                    :wednesdays 3
+                    :thursdays 4
+                    :fridays 5
+                    :saturdays 6
+                    :sundays 7 })
--- src/reminder_parsing.clj
+++ src/reminder_parsing.clj
@@ -33,19 +33,19 @@
   (and (not (comment-line? s))
   (not (blank-line? s))))
 
-(def day-names ["Sunday" "Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday"])
-(def ordinals (map ordinalize (range 1 32)))
+(let [day-names ["Sunday" "Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday"]
+      ordinals (map ordinalize (range 1 32))]
 
-(def everyday-regex #"(?i)^\s*Every ?day\s*$")
-(def every-x-weeks-regex (re-pattern (str "(?i)^\\s*Every\\s+(" (join "|" ordinals) ")\\s+(" (join "|" day-names) "),?\\s+starting\\s+(\\d{1,2})\\/(\\d{1,2})\\/(\\d{4})\\s*$")))
-(def every-x-days-regex   (re-pattern (str "(?i)^\\s*Every\\s+(" (join "|" ordinals) ")\\s+day,?\\s+starting\\s+(\\d{1,2})\\/(\\d{1,2})\\/(\\d{4})\\s*$")))
-(def date-regex      #"(?i)^\s*on\s+(\d{1,2})/(\d{1,2})/(\d{4})\s+")
-(def month+day-regex #"(?i)^\s*on\s+(\d{1,2})/(\d{1,2})\s+")
-(def day-of-week-regex #"(?i)(mondays|tuesdays|wednesdays|thursdays|fridays|saturdays|sundays)")
-(def day-of-month-identifier-regex #"(?i)^\s*Every (.+) of the month\s*$")
-(def ordinal-regex #"(?i)\d+(st|nd|rd|th)")
+  (def everyday-regex #"(?i)^\s*Every ?day\s*$")
+  (def every-x-weeks-regex (re-pattern (str "(?i)^\\s*Every\\s+(" (join "|" ordinals) ")\\s+(" (join "|" day-names) "),?\\s+starting\\s+(\\d{1,2})\\/(\\d{1,2})\\/(\\d{4})\\s*$")))
+  (def every-x-days-regex (re-pattern (str "(?i)^\\s*Every\\s+(" (join "|" ordinals) ")\\s+day,?\\s+starting\\s+(\\d{1,2})\\/(\\d{1,2})\\/(\\d{4})\\s*$")))
+  (def date-regex #"(?i)^\s*on\s+(\d{1,2})/(\d{1,2})/(\d{4})\s+")
+  (def month+day-regex #"(?i)^\s*on\s+(\d{1,2})/(\d{1,2})\s+")
+  (def day-of-week-regex #"(?i)(mondays|tuesdays|wednesdays|thursdays|fridays|saturdays|sundays)")
+  (def day-of-month-identifier-regex #"(?i)^\s*Every (.+) of the month\s*$")
+  (def ordinal-regex #"(?i)\d+(st|nd|rd|th)")
 
-(def days-in-advance-regex #"(?i)^\s*notify\s+(\d+)\s+days?\s+in\s+advance\s*$")
+  (def days-in-advance-regex #"(?i)^\s*notify\s+(\d+)\s+days?\s+in\s+advance\s*$"))
 
 (def ^{:private true} default-days-in-advance 1)
 
