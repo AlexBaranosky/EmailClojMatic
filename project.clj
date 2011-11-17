af952ee8c41f1e878c771eaa7ad2f3586ee275e7
Alex Baranosky <alexander.baranosky@gmail.com>
2011-11-16 21:15:18 -0500

    upgraded to 1.3

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject emailclojmatic "0.9.0-alpha"
   :description "Periodic email reminders"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [joda-time "2.0"]
                  [utilize "0.1.2"]
                  [slingshot "0.8.0"]
                  [org.apache.commons/commons-email "1.2"]
                  [fs "0.9.0"]]
-  :dev-dependencies [[midje "1.2-alpha4"]]
+  :dev-dependencies [[midje "1.3-alpha5"]]
   :main email-cloj-matic)
