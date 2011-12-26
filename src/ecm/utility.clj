9ade39b204db5dd9ea80063a53c1c58f98164d47
Alex Baranosky <alexander.baranosky@gmail.com>
2011-12-26 18:22:34 -0500

    resource directory is now hardcodedin a  resource-info.cljdata file, this way I can get the Files for the resources and write to them, instead of just having read-only access to them.

--- src/ecm/utility.clj
+++ src/ecm/utility.clj
@@ -7,14 +7,20 @@
   "Like clojure.core/line-seq but opens f with reader.  Automatically
   closes the reader AFTER YOU CONSUME THE ENTIRE SEQUENCE."
   [f]
-  (let [read-line (fn this [^BufferedReader rdr]
-                    (lazy-seq
-                     (if-let [line (.readLine rdr)]
-                       (cons line (this rdr))
-                       (.close rdr))))]
+  (letfn [(read-line [^BufferedReader rdr]
+            (lazy-seq
+              (if-let [line (.readLine rdr)]
+                (cons line (read-line rdr))
+                (.close rdr))))]
     (read-line (reader f))))
 
+(defn resource-path []
+  (-> (.getResourceAsStream (ClassLoader/getSystemClassLoader) "resource-info.cljdata")
+      slurp
+      read-string
+      :resource-path ))
+
 (defn resource [the-file]
-  (file "resources" the-file))
+  (file (resource-path) the-file))
 
 (def fact-resource resource)
