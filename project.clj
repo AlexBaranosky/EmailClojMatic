8ad52123d191ef7988dc04460204051732513224
Alex Baranosky <alexander.baranosky@gmail.com>
2012-01-26 18:28:41 -0500

    updating versions

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   :description "Periodic email reminders"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [joda-time "2.0"]
-                 [utilize "0.1.2"]
+                 [utilize "0.2.2"]
                  [slingshot "0.8.0"]
                  [org.apache.commons/commons-email "1.2"]
                  [fs "1.0.0"]]
-  :dev-dependencies [[midje "1.3.0"]]
+  :dev-dependencies [[midje "1.3.2-SNAPSHOT"]]
   :main ecm.runapp)
